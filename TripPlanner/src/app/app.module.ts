4fdd6d99daa4ed9056152b0c7fde6997c4feea01
Hugo Vázquez Docampo <81679649+hugovazquezd@users.noreply.github.com>
2024-05-09 19:41:03 +0200

    Apis, librerías y versiones iniciales iwvg-hugo/TFM-FRONT#15

--- TripPlanner/src/app/app.module.ts
+++ TripPlanner/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from '@angular/common';
+import { BillsModule } from './bills/bills.module';
+import { MenuModule } from './menu/menu.module';
+import { SharedModule } from '../shared/shared.module';
+import { CalendarModuleR } from './calendar/calendar.module';
+import { MapModule } from './map/map.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BillsModule,
+    MenuModule,
+    SharedModule,
+    CalendarModuleR,
+    MapModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
