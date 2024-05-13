e38ced8ac1133c4c40b4d4474ad7b29343d1b382
Rodrigo de la Calle <rodricalle@usal.es>
2024-05-13 21:50:45 +0200

    Añadir estilos y funcionalidad de marcadores en el mapa

--- TripPlanner/src/app/map/map-plan/map-plan.component.ts
+++ TripPlanner/src/app/map/map-plan/map-plan.component.ts
@@ -1,13 +1,19 @@
-import { Component, ElementRef, AfterViewInit, ViewChild, OnDestroy, OnInit } from '@angular/core';
-import { Map, Marker, NavigationControl} from 'maplibre-gl';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { Map, Marker, NavigationControl, Offset, Popup } from 'maplibre-gl';
 
 @Component({
   selector: 'app-map-plan',
   templateUrl: './map-plan.component.html',
-  styleUrls: ['./map-plan.component.scss']
+  styleUrls: ['./map-plan.component.scss'],
 })
 export class MapPlanComponent implements OnInit, AfterViewInit, OnDestroy {
-
   initialState = { lng: -3.66, lat: 40.5, zoom: 14 };
 
   map: Map | undefined;
@@ -17,37 +23,60 @@ export class MapPlanComponent implements OnInit, AfterViewInit, OnDestroy {
   NavigationOptions = {
     showCompass: true,
     showZoom: true,
-    visualizePitch: true
-  }
+    visualizePitch: true,
+  };
 
   @ViewChild('map')
   private mapContainer!: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-
     this.map = new Map({
       container: this.mapContainer.nativeElement,
       style: `https://api.maptiler.com/maps/streets/style.json?key=Ke6yX0uzyu92hbElN1hl`,
       center: [this.initialState.lng, this.initialState.lat],
-      zoom: this.initialState.zoom
+      zoom: this.initialState.zoom,
     });
 
-    this.map.addControl(new NavigationControl(this.NavigationOptions), 'top-right');
+    this.map.addControl(
+      new NavigationControl(this.NavigationOptions),
+      'top-right'
+    );
 
     this.markers.push(
-      new Marker({color: "#FF0000"})
-      .setLngLat([-6.09, 40.02])
-      .addTo(this.map)
+      new Marker({ color: '#FF0000' }).setLngLat([-6.09, 40.02]).addTo(this.map)
     );
     this.markers.push(
-      new Marker({color: "#FF0000"})
-      .setLngLat([-3.6, 40.5])
-      .addTo(this.map)
+      new Marker({ color: '#FF0000' }).setLngLat([-3.66, 40.5]).addTo(this.map)
     );
     console.log(this.markers);
+    const map2 = this.map;
+
+    let markerHeight = 50,
+      markerRadius = 10,
+      linearOffset = 25;
+    let popupOffsets = {
+      top: [0, 0],
+      'top-left': [0, 0],
+      'top-right': [0, 0],
+      bottom: [0, -markerHeight],
+      'bottom-left': [
+        linearOffset,
+        (markerHeight - markerRadius + linearOffset) * -1,
+      ],
+      'bottom-right': [
+        -linearOffset,
+        (markerHeight - markerRadius + linearOffset) * -1,
+      ],
+      left: [markerRadius, (markerHeight - markerRadius) * -1],
+      right: [-markerRadius, (markerHeight - markerRadius) * -1],
+    };
+    let popup = new Popup({ className: 'my-class' })
+      .setLngLat([-3.66, 40.501])
+      .setHTML('<h1>Hello World!</h1>')
+      .setMaxWidth('300px')
+      .addTo(this.map);
   }
 
   ngOnDestroy() {
